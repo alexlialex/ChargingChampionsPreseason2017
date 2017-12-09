6dabb79c0f6c4247225679464cb04df65b4a2046
Alex Li <li.alex13285@gmail.com>
2017-12-09 15:05:17 -0800

    Starting drive
    Added drive subsystem

--- src/org/usfirst/frc/team6560/robot/Robot.java
+++ src/org/usfirst/frc/team6560/robot/Robot.java
@@ -67,6 +67,8 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void autonomousInit() {
 		autonomousCommand = chooser.getSelected();
+		System.out.println("HELLO");
+		//DDSFSDFDSFDSFSDF
 
 		/*
 		 * String autoSelected = SmartDashboard.getString("Auto Selector",
